2336be957a38042ed793c47d27226de3b5a33479
Khang8-1 <cis1700019@students.cis.edu.vn>
2025-10-28 20:21:43 +0700

    Add files via upload

---
+++ mr.robwork.py
@@ -0,0 +1,13 @@
+my_list = [2, 4, 6, 8, 10, 12]
+print(my_list)
+my_list.append(14)
+print(my_list)
+print(len(my_list))
+print("The element at index 2 is:", my_list[2])
+my_list.insert(2, 100)
+print(my_list)
+print("The element at index 2 is:", my_list[2])
+my_list.pop(1)
+print(my_list)
+print("The element at index 2 is:", my_list[2])
+
