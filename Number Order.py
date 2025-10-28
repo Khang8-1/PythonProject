b5aa0d3815a665634eb7f4601a623dd85d717b7c
Khang8-1 <cis1700019@students.cis.edu.vn>
2025-10-28 20:33:10 +0700

    Add files via upload

---
+++ Number Order.py
@@ -0,0 +1,9 @@
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+
+numbers = [num1, num2, num3]
+
+numbers.sort()
+print(numbers)
