6a100e899c9a89edee134e61871d16d5f9d32664
Khang8-1 <cis1700019@students.cis.edu.vn>
2025-10-28 20:46:25 +0700

    Add files via upload

---
+++ Tracing a list practice.py
@@ -0,0 +1,7 @@
+my_list = [10, 5, 20, 3, 1]
+my_list[0] = (my_list[0] + 10) // 2
+my_list[1] = (my_list[1] * 3) - 1
+my_list[2] = (my_list[2] - 4) * 2
+my_list[3] = (my_list[3] + 7) / 10
+my_list[4] = (my_list[4] * 5) + 20
+print(my_list)
